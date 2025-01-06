1c474c76cae969fef065bd1dda75dea1c660d6f0
Samuel Carvalho <samuelcarvalho2000@outlook.com>
2025-01-06 12:33:44 -0300

    feat: Implementação dos verbos HTTP PUT, PATCH e DELETE
    - Adicionado o uso do verbo PUT para atualização completa de recursos.
    - Implementado o verbo PATCH para atualizações parciais, com configuração adicional.
    - Utilizado o verbo DELETE para remoção de recursos.
    - Implementado retorno de status 204 (No Content) em caso de sucesso nas operações de atualização e remoção.

--- FilmesAPI/Profiles/FilmeProfile.cs
+++ FilmesAPI/Profiles/FilmeProfile.cs
@@ -9,5 +9,9 @@ public class FilmeProfile : Profile
     public FilmeProfile() 
     {
         CreateMap<CreateFilmeDto, Filme>();
+        CreateMap<UpdateFilmeDto, Filme>();
+        CreateMap<Filme, UpdateFilmeDto>();
+        CreateMap<Filme, ReadFilmeDto>();
+
     }
 }
